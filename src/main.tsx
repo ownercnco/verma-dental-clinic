412dce957615ccd6cc491f12cb1f751a03a09752
Saurav Upadhyay <sauravup041103@gmail.com>
2026-05-23 18:13:07 +0530

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { getRouter } from "./router";
 
 const router = getRouter();
 
-ReactDOM.createRoot(document.getElementById("root ")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
